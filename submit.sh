a4926b6ca26656b54fe0fac308f04df213a19456
Andrew Bell <majacow@gmail.com>
2019-05-02 16:52:46 -0700

    Added comments to submit.sh

--- submit.sh
+++ submit.sh
@@ -1,39 +1,52 @@
+#########################
+#      Andrew Bell      #
+# Input submission Prep #
+#########################
+
+
+#get directory name
 dirName=${PWD##*/}
-filename="AndrewBell-$dirName.txt"
+name="AndrewBell"
+filename="$name-$dirName.txt"
+#temp files for storing intermediate steps
 tempFile="temp.txt"
 sedFile="sedFile.txt"
-arr=(*.java)
 #copy all of the code files
 for name in *.java
 do
-    printf "/*------- File: $name -------*/\n" > "$filename"
-    cat "$name" >> "$filename"
-    #printf "/*----- End File: $name -----*/\n\n" >> "$filename"
+   printf "/*------- File: $name -------*/\n" > "$filename"
+   cat "$name" >> "$filename"
+   #printf "/*----- End File: $name -----*/\n\n" >> "$filename"
 done
 
-#handle the a.out file
+#handle the executable file
 printf "/* ---------- paste of run ----------\n\n" >> "$filename"
+#if there is an input file
 if [ -f input1 ]; then
-    for name in input*
-    do
-        printf "/* ---------- $name ----------*/\n\n" >> "$filename"
-        echo "$name"
-        java Main < "$name" > "$tempFile"
-        #put input where it is supposed to go
-        cat "$name" | while read line
-        do
-            sed -e "1 s/: /question $line endl/; t" -e "1,// s//question $line endl/" "$tempFile" > $sedFile
-            cat $sedFile > $tempFile
-        done
-        sed -i -e 's/question/:/g' "$tempFile"
-        sed -i -e 's/endl/\'$'\n/g' "$tempFile"
-        cat "$tempFile" >> "$filename"
-        rm temp.*
-        rm sedFile.txt
-        printf "\n" >> "$filename"
-    done
-
+   #get all of the input files
+   for name in input*
+   do
+      printf "/* ---------- $name ----------*/\n\n" >> "$filename"
+      echo "$name"
+      java Main < "$name" > "$tempFile"
+      #put input where it is supposed to go
+      cat "$name" | while read line
+      do
+         #look for ": " and place the input line and tag for clean up
+         sed -e "1 s/: /question $line endl/; t" -e "1,// s//question $line endl/" "$tempFile" > $sedFile
+         cat $sedFile > $tempFile
+      done
+      #fix tags from the sed
+      sed -i -e 's/question/:/g' "$tempFile"
+      sed -i -e 's/endl/\'$'\n/g' "$tempFile"
+      #put in correct file
+      cat "$tempFile" >> "$filename"
+      #clean up temp files
+      rm temp.*
+      rm sedFile.txt
+      printf "\n" >> "$filename"
+   done
 else
-    java Main >> "$filename"
+   java Main >> "$filename"
 fi
 printf "   ---------- paste of run ----------*/\n\n" >> "$filename"
